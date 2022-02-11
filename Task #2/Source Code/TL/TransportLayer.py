2430d0c182a61d73ef53420d21a16d2a8658bd73
KrishnKher <72561577+KrishnKher@users.noreply.github.com>
2022-02-11 17:52:28 +0530

    Create TransportLayer.py

---
+++ Task #2/Source Code/TL/TransportLayer.py
@@ -0,0 +1,34 @@
+import socket
+import logging
+import time
+import argparse
+import threading
+
+
+def test():
+    print("It works!")
+
+
+
+class RDT:
+
+    def __init__(self):
+
+
+    def flow_control(self, args):
+
+
+    def congestion_control(self, args): # Thinking of 2 approaches.
+
+    # Security, if time permits and if doable.
+
+class Packet(RDT):
+
+    def __init__(self):
+
+    def packet_acknowledgement(self, packet):
+
+    def packet_loss_detection(self, packet):
+
+    def packet_retransmission(self, packet):
+
