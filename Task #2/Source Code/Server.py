3cc6c045403c169676a5378ca50d79dc544c5911
chandra3000 <74546888+chandra3000@users.noreply.github.com>
2022-02-20 23:58:07 +0530

    Update Server.py

--- Task #2/Source Code/Server.py
+++ Task #2/Source Code/Server.py
@@ -1,6 +1,34 @@
+import TL.ApplicationLayer as AL
+import socket
 
+out_filename = "out"
 
+ofile = open(out_filename, 'wb')
 
-'''
-receive() function called here?
-'''
+# Creates UDP socket object.
+receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Defining the desired port to listen to on the server.
+server_IP = '192.168.1.3'
+server_port = 5000
+server_address = (server_IP, server_port)
+
+# Defining the port on the client to send ACKs to.
+client_IP = '192.168.1.3'
+client_port = 10000
+client_address = (client_IP, client_port)
+
+receive_socket.bind(server_address) # Server socket up for receiving data.
+
+print("Server is listening")
+
+ftpserver = AL.App_Interface()
+buffer = ftpserver.send_ack(receive_socket, client_address)
+
+print(len(buffer))
+
+ordered_seq = sorted(buffer.keys())
+for seq in ordered_seq:
+    ofile.write(buffer[seq])
+    
+#print(len(buffer))
